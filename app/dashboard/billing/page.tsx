206ae18bdc53c574f3c5c432572c1258b53e0072
GregJolly <gregjofficial@gmail.com>
2025-10-01 13:12:57 -0500

    Created payment failed and success pages

--- app/dashboard/billing/page.tsx
+++ app/dashboard/billing/page.tsx
@@ -65,8 +65,8 @@ export  default async function BillingPage(){
 
     return (
         <div className="max-w-md mx-auto space-y-4">
-            <Card className="flex flex-col">
-                <CardContent className="py-8">
+            <Card className="flex flex-col pb-1 ">
+                <CardContent className="py-6 ">
                    <div>
                     <h3 className="inline-flex px-4 py-1 rounded-lg bg-primary/10 tracking-wide uppercase text-sm font-semibold text-primary">monthly</h3>
 
@@ -77,7 +77,7 @@ export  default async function BillingPage(){
                    <p className="text-lg mt-4   text-muted-foreground"> Unlimited notes for $5 per month. </p>
 
                 </CardContent>
-                <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-secondary rounded-lg m-1 space-y-6 sm:p-10 sm:pt-6">
+                <div className="flex-1 flex flex-col justify-between px-6 pt-6 pb-8 bg-secondary rounded-lg m-1 space-y-8 sm:p-10 sm:pt-6">
                     <ul className="space-y-4">
                     {featureItems.map((item, index)=>(
                         <li key={index} className="flex items-center">
