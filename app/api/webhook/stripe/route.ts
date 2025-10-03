6bddd8be5345ceafcf447b98fa1c83d4d0cdb6ac
GregJolly <gregjofficial@gmail.com>
2025-10-02 20:04:44 -0500

    Building web hook

---
+++ app/api/webhook/stripe/route.ts
@@ -0,0 +1,34 @@
+import { stripe } from "@/app/lib/stripe"
+import { headers } from "next/headers"
+import Stripe from 'stripe'
+export async function POST(req: Request){
+    const body = await req.text()
+
+    const signature = (await headers()).get('Stripe-Signature') as string 
+
+    let event: Stripe.Event
+
+    try{
+        event = stripe.webhooks.constructEvent(
+           body, 
+           signature,  
+           process.env.STRIPE_WEBHOOK_SECRET as string 
+        )
+    }catch(error:unknown) {
+        return new Response("webhook error", {status: 400})
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session
+
+    if (event.type === "checkout.session.completed")
+    {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription as string
+        )
+
+        const customerId = String(session.customer)
+
+        const user = await prisma
+    }
+
+}
